c06e2917156d25195c148336f2004cf611ebb877
NikolasN <ninios.nikolas@gmail.com>
2022-02-21 21:18:42 +0200

    test1

--- ConsoleApp2/Program.cs
+++ ConsoleApp2/Program.cs
@@ -11,6 +11,9 @@ namespace ConsoleApp2
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World");
+            Console.WriteLine("Hello World");
+            Console.WriteLine("Hello World");
+            Console.WriteLine("Hello World");
         }
     }
 }
